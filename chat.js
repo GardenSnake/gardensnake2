3ff2d804f0188d4a0ac3387cbcdbdea5fea1a411
GardenSnake <LIGHTNlNG@yahoo.com>
2017-09-24 07:19:02 -0400

    initial project commit

---
+++ chat.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+http.listen(3000);
+
+app.get('/', function(req,res){
+	res.sendFile(__dirname + '/chat.html');
+});
+
+io.on('connection',function(socket){
+
+	//when 'send message' is recived on server, emit data to everyone
+	socket.on('send message', function(data){
+		io.emit('new message',data);
+	});
+
+});
