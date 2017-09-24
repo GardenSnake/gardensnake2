4ac91af46f5d0a55e1d666fa0b5098ca61867c56
GardenSnake <LIGHTNlNG@yahoo.com>
2017-09-24 07:55:51 -0400

    8080

--- chat.js
+++ chat.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-http.listen(3000);
+http.listen(8080);
 
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/chat.html');
